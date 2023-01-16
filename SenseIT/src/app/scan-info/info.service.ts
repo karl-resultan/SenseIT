a4e88d1a0a56c78546ba6e1764c72a602d8dc306
markJasonMargallo <201910681@gordoncollege.edu.ph>
2023-01-16 12:08:22 +0800

    fix adding device logic

--- SenseIT/src/app/scan-info/info.service.ts
+++ SenseIT/src/app/scan-info/info.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Router} from "@angular/router";
+import { Router } from "@angular/router";
 import { Device } from '../main/device.interface';
 
 @Injectable({
@@ -11,23 +11,29 @@ export class InfoService {
 
   constructor(private route: Router) { }
 
-  getDeviceList(): Device[]{
-    let devices :Device[] = []
-    if(localStorage.getItem('device_list')){
+  getDeviceList(): Device[] {
+    let devices: Device[] = []
+    if (localStorage.getItem('device_list')) {
       devices = JSON.parse(<string>localStorage.getItem("device_list"))
     }
     return devices;
   }
 
-  appendDevice(device: Device){
+  appendDevice(device: Device) {
 
     const devices: Device[] = this.getDeviceList();
     devices.push(device);
 
-    localStorage.setItem("device_list", JSON.stringify(devices))
+    if (devices.length > 0) {
+      localStorage.setItem("device_list", JSON.stringify(devices))
+    }else{
+      let devs: Device[] = [];
+      devs.push(device);
+      localStorage.setItem("device_list", JSON.stringify(devs))
+    }
   }
 
-  addDevice(device: Device){
+  addDevice(device: Device) {
     this.appendDevice(device)
     this.device_list.push(device);
     this.route.navigate(['/main']);
